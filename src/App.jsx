4620cad7806131835f8d2db29d42c4a229ff974e
jay26027894 <125072052+jay26027894@users.noreply.github.com>
2025-05-31 13:21:19 +0530

    Fix: allow interaction with sections by setting pointer-events none on Particles

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,38 @@ import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-
-
+// import Backgroundshapes from './components/Backgroundshapes'
+import Particles from '../components/Particles'
 function App() {
   return (
+    
     <div className='bg-black '>
       {/* <SplashCursor /> */}
+      {/* <Backgroundshapes /> */}
+   <div
+  style={{
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: 1,
+    pointerEvents: 'none', 
+  }}
+>
+  <Particles
+  cla
+    particleColors={['#FF69B4' /* Hot Pink */, '#FF1493' /* Deep Pink */]}
+    particleCount={500}
+    particleSpread={10}
+    speed={0.1}
+    particleBaseSize={100}
+    moveParticlesOnHover={true}
+    alphaParticles={true}
+    disableRotation={false}
+  />
+</div>
+
       <Navbar />
       <Hero />
       <About />
